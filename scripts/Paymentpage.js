5d851ac5ddd8341266bf46bf85fc60e0b77c317e
pranaytandel09 <pranaytandel09@gmail.com>
2023-05-06 15:43:26 +0530

    Payment page

--- scripts/Paymentpage.js
+++ scripts/Paymentpage.js
@@ -52,13 +52,10 @@ optn2.addEventListener("click",function(e){
     dynamicBox.style.display="none";
     fetchBeneficiaryData()
 })
-submitBtn1.addEventListener("click",function(e){
-e.preventDefault();
-box1.style.display="none";
-box2.style.display="block";
-box3.style.display="none";
-box4.style.display="none";
-})
+// submitBtn1.addEventListener("click",function(e){
+// e.preventDefault();
+
+// })
 submitBtn2.addEventListener("click",function(e){
     e.preventDefault();
     box1.style.display="none";
@@ -111,29 +108,233 @@ passwordButtons.forEach((button) => {
     }
   });
 });
+// Currency exchange------------------------------->
+
+let currencyRateData=[]
+
+let fromCountries= document.getElementById("fromCountries");
+let toCountries= document.getElementById("toCountries")
+let fromCurrency= document.getElementById("fromCurrency")
+let toCurrency= document.getElementById("toCurrency");
+let currency=document.getElementById("currency");
+
+fetchCurrancyRate();
+
+function fetchCurrancyRate(){
+
+  fetch("https://mock-server-fw26-217-tz58.onrender.com/Contries")
+  .then((res)=>res.json())
+  .then((data)=>{
+    console.log(data)
+  renderCountries(data)
+  currencyRateData=data;
+  })
+  .catch((err)=>console.log(err))
+}
+
+function  renderCountries(data){
+  fromCountries.innerHTML="";
+  toCountries.innerHTML="";
+  data.forEach(function(el){
+// console.log(el)
+let optn= document.createElement("option");
+optn.value= el.name;
+
+let optn2= document.createElement("option");
+optn2.value= el.name;
+
+let img= document.createElement("img")
+img.src= el.image;
+img.class= "countryImg";
+optn.innerText= el.name
+optn2.innerText= el.name
+
+
+
+
+toCountries.append(optn2)
+fromCountries.append(optn)
+  })
+}
+
+currency.addEventListener("submit",function(e){
+  e.preventDefault()
+   let inputRate=currencyRate(fromCountries.value)
+   let outputRate=currencyRate(toCountries.value)
+   let inputValue= currency.fromCurrency.value;
+ 
+let result=(outputRate / inputRate)*inputValue;
+   currency.toCurrency.value= Math.ceil(result);
+
+   let bankDetails=document.getElementById("bankDetails");
+   bankDetails.Amount.value=toCurrency.value;
+  })
+
+  function currencyRate(a){
+   let value= "";
+   currencyRateData.forEach(function(e){
+ 
+      if(e.name==a){
+       value= e.rate;
+      }
+    })
+  return(value)
+  }
+
+// Get the "from" and "to" country select elements
+
+
+
+// let fromCountries = document.getElementById("fromCountries");
+// let toCountries = document.getElementById("toCountries");
+
+// // Call the fetchCurrancyRate function to fetch data and render the country options
+// fetchCurrancyRate();
+
+// function fetchCurrancyRate() {
+//   fetch("https://mock-server-fw26-217-tz58.onrender.com/Contries")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       renderCountries(data);
+//     })
+//     .catch((err) => console.log(err));
+// }
+
+// function renderCountries(data) {
+//   // Clear the select options first
+//   fromCountries.innerHTML = "";
+//   toCountries.innerHTML = "";
+
+//   // Loop through the data and add options to the "from" and "to" country selects
+//   data.forEach(function (country) {
+//     // console.log(country)
+//     // Create a new option element with the country name as the text content
+//     let option = document.createElement("option");
+//     option.text = country.name;
+//     option.value = country.name;
+// option.style.backgroundImage=`url(${country.img})`
+//     // Create a new image element with the country flag image source
+//     let img = document.createElement("img");
+//     img.src = country.image;
+//     img.className = "countryImg";
+
+//     // Append the image to the option element
+//     option.appendChild(img);
+
+//     // Add the option to the "from" and "to" country selects
+//     fromCountries.appendChild(option.cloneNode(true));
+//     toCountries.appendChild(option);
+//   });
+
+
+//   console.log($('#fromCountries'));
+// $('#fromCountries').select2();
+
+//   // Initialize the select2 plugin on the "from" and "to" country selects
+//   $("#fromCountries, #toCountries").select2({
+//     templateResult: function (result) {
+//       if (!result.id) {
+//         return "";
+//       }
+//       var $div = $('<div class="country-option">');
+//       var $img = $('<img>');
+//       $img.attr('src', result.element.dataset.image);
+//       $img.addClass('country-img');
+//       $div.append($img);
+//       $div.append(result.text);
+//       return $div;
+//     },
+//     dropdownCssClass: 'country-dropdown',
+//     minimumResultsForSearch: Infinity
+//   });
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Tickmark Animation ------------------------------>
-function animationTickmark() {
-    const tickAnimation = document.getElementById("tickAnimation");
-    tickAnimation.classList.add("show");
-  }
+// function animationTickmark() {
+//     const tickAnimation = document.getElementById("tickAnimation");
+//     tickAnimation.classList.add("show");
+//   }
   
-  document.addEventListener("DOMContentLoaded", animationTickmark);
+//   document.addEventListener("DOMContentLoaded", animationTickmark);
 
-// -------------------------------------------------------->
+// // -------------------------------------------------------->
 
-// sending money to benificary
-// Replace this with the URL of your API endpoint
-const API_URL = "https://reqres.in/api/users";
+// // sending money to benificary
+// // Replace this with the URL of your API endpoint
+// const API_URL = "https://reqres.in/api/users";
 
-// Get the beneficiary select element and the beneficiary list div
-const beneficiarySelect = document.getElementById("beneficiary");
-const beneficiaryList = document.getElementById("beneficiaryList");
+// // Get the beneficiary select element and the beneficiary list div
+// const beneficiarySelect = document.getElementById("beneficiary");
+// const beneficiaryList = document.getElementById("beneficiaryList");
 
-// Fetch the beneficiary data from the API
-function fetchBeneficiaryData(){
+// // Fetch the beneficiary data from the API
+// function fetchBeneficiaryData(){
 
 
+// }
+// ____________________________________________________________________________________________
+
+
+// bankDetails form submission------------------------>
+let transactionDetails= JSON.parse(localStorage.getItem("transaction")) || []
+let bankDetails=document.getElementById("bankDetails");
+
+bankDetails.addEventListener("submit",function(e){
+  e.preventDefault();
+ 
+if(bankDetails.bankName.value=="" || bankDetails.accNo.value=="" || bankDetails.recipientName.value==""){
+  alert("Enter All Feilds")
 }
+else{
+  let obj={};
 
+  obj.currencyName=toCountries.value;
+  
+ 
+  obj.amount= bankDetails.Amount.value;
+  obj.bankName=bankDetails.bankName.value;
+  obj.accNo= bankDetails.accNo.value;
+  obj.recipientName= bankDetails.recipientName.value;
+  box1.style.display="none";
+box2.style.display="block";
+box3.style.display="none";
+box4.style.display="none";
+console.log(obj)
+transactionDetails.push(obj)
+localStorage.setItem("transaction",JSON.stringify(transactionDetails))
+
+tansactionCheck(obj)
+}
+  
+})
+
+// Review your transaction details section
+
+let amountDisplay= document.getElementById("amountDisplay")
+let checkValue= document.getElementById("checkValue")
+let checkName=document.getElementById("checkName")
+let checkBank= document.getElementById("checkBank")
+let transactionAmount=document.getElementById("transactionAmount")
+let transactionFees=document.getElementById("transactionFees")
+
+function tansactionCheck(obj){
+
+  checkValue.innerText=`${obj.currencyName} ${Number(obj.amount) + Math.floor(Math.random()*70)}` 
+  checkName.innerText=`${obj.recipientName}`
+  checkBank.innerText=`${obj.bankName} | ${obj.accNo}` 
+
+  transactionAmount.innerText=obj.amount;
+  transactionFees.innerText=Math.floor(Math.random()*70);
+}
