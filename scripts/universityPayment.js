9e5616aac7de0aacb8e9b610c1cbb16d23b90ad1
Anburaj <anburaj2203@gmail.com>
2023-05-08 20:41:14 +0530

    updated cardpages,universitypages,homepage

--- scripts/universityPayment.js
+++ scripts/universityPayment.js
@@ -8,33 +8,36 @@ let transactionFees = document.getElementById("transactionFees");
 
 let universityData = JSON.parse(localStorage.getItem("university-data")) || [];
 let data = universityData[universityData.length - 1];
+// toCountry,
+//     fromCountry,
+//     from_Currency,
+//     to_Currency,
+//     recevierName,
+//     payInfo,
 console.log(data.payInfo.join(","));
-
-checkValue.textContent = `${data.currencyName} ${data.amount}`;
+let fromCountry=data.fromCountry;
+checkValue.textContent = `${data.toCountry} ${data.to_Currency}`;
 // checkName.textContent = data.userName;
 checkUniversity.textContent = `${data.recevierName}`;
 checkPayinfo.textContent = data.payInfo.join(",");
-transactionAmount.textContent = `${data.currencyName} ${data.amount}`;
-let charges = Math.floor(Math.random() * 40);
-transactionFees.textContent = `${data.currencyName} ${charges}`;
+transactionAmount.textContent = `${data.toCountry} ${data.to_Currency}`;
+let charges = Math.floor(Math.random() * 10);
+transactionFees.textContent = `${fromCountry} ${charges}`;
 
-let userName=data.userName;
-let receiverName=data.recevierName;
-let currencyName=data.currencyName;
-let convertedAmount=data.amount;
-let charge=charges;
-let originalAmount=data.originalAmount;
-let total= Number(convertedAmount)+charge;
+let recepientName=data.recevierName;
+let fromCurrencyName=data.fromCountry;
+let sentAmount= Number(data.to_Currency);
+let debitableAmount= Number(data.from_Currency)+Number(charges);
+let toCurrencyName=data.toCountry;
+// let total= Number(convertedAmount)+charge;
 let obj={
-    userName,
-    receiverName,
-    currencyName,
-    convertedAmount,
-    originalAmount,
-    charge,
-    total
+  debitableAmount,    
+    fromCurrencyName,
+    recepientName,
+    sentAmount,
+    toCurrencyName
 }
-
+let box2 = document.getElementById("box2");
 let box3 = document.getElementById("box3");
 let submitBtn2 = document.getElementById("submitBtn2");
 submitBtn2.addEventListener("click", function (e) {
@@ -62,8 +65,10 @@ passwordButtons.forEach((button) => {
       if (password === "5872") {
         showAlert()
         function showAlert() {
+          box2.style.display='none';
+          box3.style.display='none'
 			document.getElementById("alert").classList.add("show");
-			setTimeout(hideAlert, 3000);
+			setTimeout(hideAlert, 4000);
 		}
 		function hideAlert(){
 			document.getElementById("alert").classList.remove("show");
@@ -71,10 +76,7 @@ passwordButtons.forEach((button) => {
 			setTimeout(function() {
 				document.getElementById("alert").classList.remove("hide");
 			}, 2000);
-            let balacneData=localStorage.getItem('balance-data')
-            balacneData-=originalAmount;
-            localStorage.setItem('balance-data',balacneData)
-            window.location.href="Homepage.html"
+            window.location.href="Homepage.html";
 		}
       } else {
         alert("Password is incorrect!");
@@ -84,7 +86,9 @@ passwordButtons.forEach((button) => {
     }
   });
 });
-let universityTransaction=JSON.parse(localStorage.getItem('university-transaction'))||[];
-// console.log(obj)
-universityTransaction.push(obj);
-localStorage.setItem('university-transaction',JSON.stringify(universityTransaction))
+
+console.log(obj)
+
+let transactionDetails= JSON.parse(localStorage.getItem("transaction")) ||[];
+transactionDetails.push(obj);
+localStorage.setItem('transaction',JSON.stringify(transactionDetails))
